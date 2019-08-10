7ef19674710073c7aa6aab373ade18d594913cbb
chuckki <chuckki@projektorientiert.de>
2019-08-10 12:45:44 +0200

    dev commit

--- src/Controller/FrameController.php
+++ src/Controller/FrameController.php
@@ -5,8 +5,10 @@ namespace Chuckki\HvzIframeBundle\Controller;
 use Chuckki\ContaoHvzBundle\HvzModel;
 use Contao\CoreBundle\Framework\ContaoFramework;
 use Contao\InsertTags;
+use Haste\DateTime\DateTime;
 use Haste\Form\Form;
 use Haste\Input\Input;
+use Http\Discovery\Exception\NotFoundException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Session;
@@ -35,88 +37,112 @@ class FrameController extends Controller
         $orderObj   = HvzModel::findById($id);
         $formString = $this->buildForm($orderObj);
 
-        return $this->render(
-            '@ChuckkiHvzIframe/frame.details.html.twig',
-            [
-                'hvz'     => $orderObj,
-                'hvzForm' => $formString->generate(),
-                'requestToken' => \RequestToken::get(),
-                'formId'  => 'hvzOrderform'
-            ]
-        );
+        return $this->render('@ChuckkiHvzIframe/frame.details.html.twig', [
+            'hvz'          => $orderObj,
+            'hvzForm'      => $formString->generate(),
+            'requestToken' => \RequestToken::get(),
+            'formId'       => 'hvzOrderform'
+        ]);
     }
+
+    public function getHvbPrice($id): Response
+    {
+        $startDateString = Input::post('startDate');
+
+        $extraTag  = (int)Input::post('extraTag') - 1;
+        $hvzType   = Input::post('hvzType');
+
+        setlocale(LC_ALL, 'de_DE');
+        $startDate = \DateTime::createFromFormat('d.m.Y', $startDateString);
+        $endDate   = \DateTime::createFromFormat('d.m.Y', $startDateString);
+        $endDate   = $endDate->modify('+' . $extraTag . ' days');
+        $startTag  = strftime('%A', $startDate->getTimestamp());
+        $endTag    = strftime('%A', $endDate->getTimestamp());
+        $hvzObj    = HvzModel::findById($id);
+
+        if (!$hvzObj) {
+            throw new NotFoundException('Hvz not found');
+        }
+
+        $price = $hvzObj->hvz_single;
+
+        if ($hvzType === 'beidseitig') {
+            $price = $hvzObj->hvz_double;
+        }
+
+
+        $order = [
+            'ort'            => $hvzObj->question,
+            'startDateName'  => $startTag,
+            'startDateValue' => $startDate->format('d.m.Y'),
+            'endDateName'    => $endTag,
+            'endDateValue'   => $endDate->format('d.m.Y'),
+            'durationDay'    => $extraTag + 1,
+            'priceHvz' =>$price,
+            'priceFull' => $price + ($hvzObj->hvz_extra_tag * $extraTag),
+            'hvz'   => $hvzObj,
+            'day'            => (($extraTag + 1) === 1) ? 'Tag' : 'Tage'
+        ];
+
+        return $this->render('@ChuckkiHvzIframe/overviewOrder.html.twig', [
+            'order' => $order
+        ]);
+
+    }
+
 
     private function buildForm(HvzModel $hvzModel)
     {
-        $objForm = new Form(
-            'hvzOrderform', 'POST', function ($objHaste) {
+        $objForm = new Form('hvzOrderform', 'POST', function ($objHaste) {
             return Input::post('FORM_SUBMIT') === $objHaste->getFormId();
-        }
-        );
+        });
 
-        $objForm->addFormField('hvzId',
-            array(
-                'default'     => $hvzModel->id,
-                'label'       => 'hvzId',
-                'inputType'   => 'hidden',
-                'eval'        => array('mandatory' => true)
-            )
-        );
+        $objForm->addFormField('hvzId', array(
+            'default'   => $hvzModel->id,
+            'label'     => 'hvzId',
+            'inputType' => 'hidden',
+            'eval'      => array('mandatory' => true)
+        ));
 
         // Ort
-        $objForm->addFormField(
-            'ort',
-            array(
-                'default'     => $hvzModel->question,
-                'label'       => 'Ort',
-                'inputType'   => 'text',
-                'eval'        => array('mandatory' => true, 'readonly' => true)
-            )
-        );
+        $objForm->addFormField('ort', array(
+            'default'   => $hvzModel->question,
+            'label'     => 'Ort',
+            'inputType' => 'text',
+            'eval'      => array('mandatory' => true, 'readonly' => true)
+        ));
         // PLZ
-        $objForm->addFormField(
-            'plz',
-            array(
-                'label'     => 'PLZ',
-                'inputType' => 'text',
-                'eval'      => array('mandatory' => true, 'rgxp' => 'digit', 'maxlength' => 5, 'minlength' => 4)
-            )
-        );
+        $objForm->addFormField('plz', array(
+            'label'     => 'PLZ',
+            'inputType' => 'text',
+            'eval'      => array('mandatory' => true, 'rgxp' => 'digit', 'maxlength' => 5, 'minlength' => 4)
+        ));
         // Strasse
-        $objForm->addFormField(
-            'strasse',
-            array(
-                'label'     => 'Straße und Hausnumer',
-                'inputType' => 'text',
-                'eval'      => array('mandatory' => true)
-            )
-        );
+        $objForm->addFormField('strasse', array(
+            'label'     => 'Straße und Hausnumer',
+            'inputType' => 'text',
+            'eval'      => array('mandatory' => true)
+        ));
         // Need a checkbox?
-        $objForm->addFormField(
-            'termsOfUse',
-            array(
-                'label'     => array('', 'Einverstanden mit den AGB'),
-                'inputType' => 'checkbox',
-                'eval'      => array('mandatory' => true)
-            )
-        );
+        $objForm->addFormField('termsOfUse', array(
+            'label'     => array('', 'Einverstanden mit den AGB'),
+            'inputType' => 'checkbox',
+            'eval'      => array('mandatory' => true)
+        ));
 
         // Let's add  a submit button
-        $objForm->addFormField(
-            'submit',
-            array(
-                'label'     => 'Submit form',
-                'inputType' => 'submit'
-            )
-        );
+        $objForm->addFormField('submit', array(
+            'label'     => 'Submit form',
+            'inputType' => 'submit'
+        ));
         return $objForm;
     }
 
 
     public function checkFormAction(): Response
     {
-        $hvzId = \Input::post('hvzId');
-        $objHvz = HvzModel::findById($hvzId);
+        $hvzId   = \Input::post('hvzId');
+        $objHvz  = HvzModel::findById($hvzId);
         $objForm = $this->buildForm($objHvz);
         if ($objForm->validate()) {
             $arrData = $objForm->fetchAll();
