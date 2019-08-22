c5fb20fe99752944f36979c65abc1753908928f7
chuckki <chuckki@projektorientiert.de>
2019-08-22 15:30:18 +0200

    add AccessDeniedException

--- src/Services/Authenticator.php
+++ src/Services/Authenticator.php
@@ -4,32 +4,37 @@
 namespace Chuckki\HvzIframeBundle\Services;
 
 
+use Contao\CoreBundle\Exception\AccessDeniedException;
+
 class Authenticator
 {
 
-    private $frameOptions;
+    private $iframe_user;
 
     /**
      * Authenticator constructor.
      */
-    public function __construct($frameOptions)
+    public function __construct($iframe_user)
     {
-        $this->frameOptions = $frameOptions;
+        $this->iframe_user = $iframe_user;
     }
 
     public function getUsers()
     {
-        dump($this->frameOptions);
+        dump($this->iframe_user);
     }
 
     public function getApiPwForUser($customer)
     {
-
+        return $this->iframe_user[$customer];
     }
 
     public function isUserAuth($customer)
     {
-        return true;
+        if(!array_key_exists($customer, $this->iframe_user))
+        {
+            throw new AccessDeniedException('Access Denied');
+        }
     }
 
 }
